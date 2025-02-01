0e46ec3abfea617f4a1913bde117bf48214bb851
Sadegh 😋 <m.sosahabi@datisint.com>
2025-02-01 11:30:12 +0330

    WIP:  performance

--- components/skills/key.vue
+++ components/skills/key.vue
@@ -1,20 +1,29 @@
-<script setup lang="ts">
-const skills = ref<KeySkill[]>([
-    { title: "CMS", fields: ["Wordpress", "Joomla", "Strapi"] },
-    { title: "Mobile", fields: ["Android", "Flutter"] },
-    { title: "Server configuration and SysAdmin", fields: ["Linux", "Nginx", "Docker"] },
-    { title: "Database Administration", fields: ["Mysql", "SQL Server", "MongoDB", "Oracle"] },
-    { title: "Bakend development", fields: ["NodeJs", "ExpressJs", "NestJs", "PHP", "Laravel", "Go"] },
-    { title: "Front development", fields: ["VueJs", "ReactJs", "NuxtJs", "NextJs}"] }
-]);
-</script>
 <template>
     <section class="flex flex-col gap-3 print:gap-1">
         <h2 class="text-2xl print:text-sm font-bold mb-2"><a href="#key-skills">Key Skills</a></h2>
-        <div v-for="(skill, index) in skills" :key="`KeySkillCat${index}`"
-            class="flex flex-col md:flex-row items-start md:items-center gap-2 pl-6">
-            <h3 class="font-bold print:text-base">{{ skill.title }}: </h3>
-            <h6 class="text-sm print:text-xs">{{ skill.fields.join(', ') }}</h6>
+        <div class="flex flex-col md:flex-row items-start md:items-center gap-2 pl-6">
+            <h3 class="font-bold print:text-base">CMS: </h3>
+            <h6 class="text-sm print:text-xs">"Wordpress, Joomla, Strapi"</h6>
+        </div>
+        <div class="flex flex-col md:flex-row items-start md:items-center gap-2 pl-6">
+            <h3 class="font-bold print:text-base">Mobile: </h3>
+            <h6 class="text-sm print:text-xs">"Android, Flutter"</h6>
+        </div>
+        <div class="flex flex-col md:flex-row items-start md:items-center gap-2 pl-6">
+            <h3 class="font-bold print:text-base">Server configuration and SysAdmin: </h3>
+            <h6 class="text-sm print:text-xs">"Linux, Nginx, Docker"</h6>
+        </div>
+        <div class="flex flex-col md:flex-row items-start md:items-center gap-2 pl-6">
+            <h3 class="font-bold print:text-base">Database Administration: </h3>
+            <h6 class="text-sm print:text-xs">"Mysql, SQL Server, MongoDB, Oracle"</h6>
+        </div>
+        <div class="flex flex-col md:flex-row items-start md:items-center gap-2 pl-6">
+            <h3 class="font-bold print:text-base">Bakend development: </h3>
+            <h6 class="text-sm print:text-xs">"NodeJs, ExpressJs, NestJs, PHP, Laravel, Go"</h6>
+        </div>
+        <div class="flex flex-col md:flex-row items-start md:items-center gap-2 pl-6">
+            <h3 class="font-bold print:text-base">Front development: </h3>
+            <h6 class="text-sm print:text-xs">"VueJs, ReactJs, NuxtJs, NextJs</h6>
         </div>
     </section>
 </template>
--- components/skills/soft.vue
+++ components/skills/soft.vue
@@ -1,18 +1,25 @@
-<script setup lang="ts">
-const skills = ref<SoftSkill[]>([
-    { title: "Problem Solving", description: "Adept at troubleshooting and finding effective solutions to technical challenges." },
-    { title: "Team Collaboration", description: "Strong ability to work in diverse teams and communicate effectively." },
-    { title: "Leadership", description: "Experienced in mentoring and leading teams towards successful project outcomes." },
-    { title: "Adaptability", description: "Quick to learn and implement new technologies and methodologies." },
-]);
-</script>
 <template>
     <section class="flex flex-col gap-3 print:gap-1">
         <h2 class="text-2xl print:text-sm font-bold mb-2"><a href="#soft-skills">Soft Skills</a></h2>
-        <div v-for="(skill, index) in skills" :key="`SoftSkillCat${index}`"
-            class="flex flex-col md:flex-row items-start md:items-center gap-2 pl-6">
-            <h3 class="font-bold print:text-base">{{ skill.title }}: </h3>
-            <span class="text-sm print:text-xs text-justify">{{ skill.description }}</span>
+        <div class="flex flex-col md:flex-row items-start md:items-center gap-2 pl-6">
+            <h3 class="font-bold print:text-base">Problem Solving: </h3>
+            <span class="text-sm print:text-xs text-justify">Adept at troubleshooting and finding effective solutions to
+                technical challenges.</span>
+        </div>
+        <div class="flex flex-col md:flex-row items-start md:items-center gap-2 pl-6">
+            <h3 class="font-bold print:text-base">Team Collaboration: </h3>
+            <span class="text-sm print:text-xs text-justify">Strong ability to work in diverse teams and communicate
+                effectively.</span>
+        </div>
+        <div class="flex flex-col md:flex-row items-start md:items-center gap-2 pl-6">
+            <h3 class="font-bold print:text-base">Leadership: </h3>
+            <span class="text-sm print:text-xs text-justify">Experienced in mentoring and leading teams towards
+                successful project outcomes.</span>
+        </div>
+        <div class="flex flex-col md:flex-row items-start md:items-center gap-2 pl-6">
+            <h3 class="font-bold print:text-base">Adaptability: </h3>
+            <span class="text-sm print:text-xs text-justify">Quick to learn and implement new technologies and
+                methodologies.</span>
         </div>
     </section>
 </template>
